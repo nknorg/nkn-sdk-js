0987a132c8e62027972240897ae250e6c5cccf22
Yilun <zyl.skysniper@gmail.com>
2020-04-13 15:37:27 -0700

    Add native support for ES6 import
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 'use strict';
 
-export { ready } from 'libsodium-wrappers';
+import { ready } from 'libsodium-wrappers';
+
+import * as nkn from './common';
+import Client from './client';
+import MultiClient from './multiclient';
+import Wallet from './wallet';
+
+nkn.ready = ready;
+nkn.Client = Client;
+nkn.MultiClient = MultiClient;
+nkn.Wallet = Wallet;
+
+export default nkn;
 
 export * from './common';
-export Client from './client';
-export MultiClient from './multiclient';
-export Wallet from './wallet';
+export { ready, Client, MultiClient, Wallet };
