a6692382c102261ff845725e7eb4c19d8687fc11
Yilun <zyl.skysniper@gmail.com>
2020-06-30 23:45:47 -0700

    Catch and print send ack error
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- dist/nkn.js
+++ dist/nkn.js
@@ -10254,7 +10254,9 @@ class MultiClient {
           if (!responded) {
             for (let clientID of Object.keys(clients)) {
               if (clients[clientID].isReady) {
-                clients[clientID]._sendACK(util.addIdentifierPrefixAll(src, clientID), messageId, isEncrypted);
+                clients[clientID]._sendACK(util.addIdentifierPrefixAll(src, clientID), messageId, isEncrypted).catch(e => {
+                  console.log('Send ack error:', e);
+                });
               }
             }
           }
