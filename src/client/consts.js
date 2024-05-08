595f167e3f98d0c0311cbe83845754cfc89772c0
Yilun <zyl.skysniper@gmail.com>
2024-05-08 00:41:14 -0700

    add multiple default stun server address
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- src/client/consts.js
+++ src/client/consts.js
@@ -7,8 +7,12 @@ export const defaultOptions = {
   msgHoldingSeconds: 0,
   encrypt: true,
   rpcServerAddr: "https://mainnet-rpc-node-0001.nkn.org/mainnet/api/wallet",
+  stunServerAddr: [
+    "stun:stun.l.google.com:19302",
+    "stun:stun.cloudflare.com:3478",
+    "stun:stunserver.stunprotocol.org:3478",
+  ],
   worker: false,
-  stunServerAddr: "stun:stun.l.google.com:19302",
 };
 
 export const defaultPublishOptions = {
